d109db1a1b0327f34c33a72ff62b82b405adcce8
Waldas3000 <waldemar.wisniewski@me.com>
2022-07-16 10:21:09 +0200

    config

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  componentDidMount() {
+    $(function() {
+      $.ajax({
+        url: "https://yuulye.herokuapp.com/data",
+        success: data => {
+          let brackets = {
+            teams : [],
+            results : []
+          };
+          if (data.type === 'unlocked') {
+            const count = data.content.count;
+            let teamCount = 1;
+            for (; teamCount < count; teamCount*=2) {
+              console.log(teamCount, count);
+            }
+            console.log('round# ', teamCount);
+            for (let i = 0; i < teamCount; i+=2) {
+              brackets.teams.push([]);
+            }
+            let half = teamCount / 2;
+            let indexStart = 0;
+            let indexHalf = half;
+            for (let i = 0; i < half; i++) {
+              brackets.teams[i].push([`Spot #${indexStart+1}`]);
+              brackets.teams[i].push([`Spot #${indexHalf+1}`]);
+              indexStart++;
+              indexHalf++;
+            }
+          } else if (data.type === 'locked') {
+            const count = data.content.count;
+            const teams = data.content.teams;
+            let teamCount = 1;
+            for (; teamCount < count; teamCount*=2) {
+              console.log(teamCount, count);
+            }
+            console.log('round# ', teamCount);
+            for (let i = 0; i < teamCount; i+=2) {
+              brackets.teams.push([]);
+            }
+            let half = teamCount / 2;
+            let indexStart = 0;
+            let indexHalf = half;
+            for (let i = 0; i < half; i++) {
+              brackets.teams[i].push(teams[indexStart]);
+              if (!teams[indexHalf]) {
+                teams[indexHalf] = null;
+              } 
+              brackets.teams[i].push(teams[indexHalf]);
+              indexStart++;
+              indexHalf++;
+            }
+          }
+          $('#bracket').bracket({
+            teamWidth: 128,
+            init: brackets,
+          });
+        },
+        error: e => {
+          console.log('error', e);
+        },
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div id="bracket"/>
+    );
+  }
+}
+
+export default App;
